4b777f7ba4ac637d742e7eff30816277c4af61f8
Lon Beshiri <lonbeshiri@gmail.com>
2023-05-23 10:04:21 -0400

    feat: app skeleton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,38 @@
+import Helmet from '@sone-dao/tone-react-helmet'
+import Sidebar from '@sone-dao/tone-react-sidebar'
+import UserController from '@sone-dao/tone-react-user-controller'
 import React from 'react'
-//import './global.scss'
+import './global.scss'
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode
+  children: React.ReactNode
 }) {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="UTF-8" />
-				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap"
-					rel="stylesheet"
-				/>
-			</head>
-			<body>
-				<main>{children}</main>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+        <script
+          src="https://kit.fontawesome.com/db877d7948.js"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body>
+        <UserController>
+          <Helmet />
+          <Sidebar />
+          <main>{children}</main>
+        </UserController>
+      </body>
+    </html>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { ServerComponent } from '@sone-dao/tone-template-react'
+
 export default function HomePage() {
-	return <p>Sup, universe?</p>
+  return <ServerComponent text="Sup, universe?" />
 }
